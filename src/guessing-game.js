db3946672e24b25525050b3ac0b4756f2bffb8e6
ShotAlex <shot.aleks@gmail.com>
2019-10-17 01:35:36 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        // (first + last) / 2   -->     binary search (wiki)
+        this.first = null;
+        this.last = null;
+    } 
 
     setRange(min, max) {
-
+        this.first = min;
+        this.last = max;
     }
 
     guess() {
-
+        return Math.ceil((this.first + this.last) / 2);
     }
 
     lower() {
-
+        this.last = this.guess();
     }
 
     greater() {
-
+        this.first = this.guess();
     }
+
 }
 
 module.exports = GuessingGame;
